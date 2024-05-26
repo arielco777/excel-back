f0eda7af6e2b45054d9872d253c4da912c95163f
Ariel Cohen <arielco777@gmail.com>
2024-05-26 18:53:12 -0400

    new test

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,28 @@ import cors from "cors";
 import { excelRouter } from "./excel/excelController";
 
 const app = express();
-app.use(
-    cors({
-        origin: "https://excel-front-lovat.vercel.app/",
-    })
-);
+const allowedOrigins = ["https://excel-front-lovat.vercel.app"];
 
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.use("/excel", excelRouter);
+app.use("/excel", cors(corsOptions), excelRouter);
 
 const port = 5000;
 app.listen(port, () => {
